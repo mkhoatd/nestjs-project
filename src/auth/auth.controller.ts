b0326a0cb0791edd4291e697a8cb417755e8e737
mkhoatd <binnhatle@gmail.com>
2022-08-28 15:54:20 +0700

    test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createAt: true,
+      },
+    });
+    delete user.id;
+    return user;
+  }
+  @Post('signin')
+  signin() {
+    return { msg: 'ccc' };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,14 @@
 // eslint-disable-next-line prettier/prettier
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+  signin() {
+    return { msg: 'Signed in' };
+  }
+  signup() {
+    return { msg: 'Signed up' };
+  }
+}
